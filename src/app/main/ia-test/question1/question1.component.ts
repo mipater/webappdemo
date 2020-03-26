55fc345cecb0f6eaf894b5ea4b00400430fd71c6
Michele Paternoster <michele.paternoster@finconsgroup.com>
2020-03-26 19:07:31 +0100

    structuring

--- src/app/main/ia-test/question1/question1.component.ts
+++ src/app/main/ia-test/question1/question1.component.ts
@@ -3,8 +3,7 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-question1',
   templateUrl: './question1.component.html',
-  styleUrls: ['./question1.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./question1.component.css']
 })
 export class Question1Component implements OnInit {
 
