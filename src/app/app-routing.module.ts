3b17d3d477259a4570254672c5101469033b169d
Michele Paternoster <michele.paternoster@finconsgroup.com>
2020-03-26 18:33:26 +0100

    structuring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,13 @@ import {IaTestComponent} from './main/ia-test/ia-test.component';
 import {InformativaComponent} from './main/informativa/informativa.component';
 import {Question1Component} from './main/ia-test/question1/question1.component';
 import {HometestComponent} from './main/ia-test/hometest/hometest.component';
+import {CanDeactivateGuard} from './main/ia-test/can-deactivate-guard.service';
 
 
 const appRoutes = [
   {path: '', component: HomeComponent},
   {path: 'privacy', component: InformativaComponent},
-  {path: 'test', component: IaTestComponent, children: [
+  {path: 'test', canDeactivateChild: [CanDeactivateGuard], component: IaTestComponent, children: [
       {path: '', component: HometestComponent},
       {path: '1', component: Question1Component},
     ]},
