a410941cea2f2ea1ffb5b845b2df7ee0c1fede69
Michele Paternoster <michele.paternoster@finconsgroup.com>
2020-05-19 18:35:51 +0200

    continue

--- src/app/services/wizard.service.ts
+++ src/app/services/wizard.service.ts
@@ -1,10 +1,12 @@
 import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class WizardService {
-  private wizardAnswers = [];
+  private wizardAnswers = new Map<number, string>();
+  recipesChanged = new Subject<string>();
 
-  addQuestionAnswer(answer) {
-    this.wizardAnswers.push(answer);
+  addQuestionAnswer(key: number, value: string) {
+    this.wizardAnswers.set(key, value)
   }
 }
