3f5fddde8f44204a661dd73711053aa00ecc5aa3
Michele Paternoster <michele.paternoster@finconsgroup.com>
2020-08-03 20:04:32 +0200

    continue working

--- src/app/main/ia-test/stepwizard/stepwizard.component.ts
+++ src/app/main/ia-test/stepwizard/stepwizard.component.ts
@@ -5,6 +5,7 @@ import {CanComponentDeactivate} from '../can-deactivate-guard.service';
 import {Observable} from 'rxjs';
 import {ModalService} from '../modal.service';
 import {WizardComponent} from 'angular-archwizard';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-stepwizard',
@@ -46,7 +47,8 @@ export class StepwizardComponent implements OnInit, CanComponentDeactivate {
         weight: new FormControl(null, [Validators.required, Validators.pattern('([4-8][0-9]|9[0-9]|[12][0-9]{2}|300)')]),
         height: new FormControl(null, [Validators.required, Validators.pattern('(8[0-9]|9[0-9]|1[0-9]{2}|2[0-4][0-9]|250)')]),
         bodyType: new FormControl(null, Validators.required)
-      })
+      }),
+      primaryObjective: new FormControl(null, Validators.required)
     });
   }
 
@@ -67,6 +69,26 @@ export class StepwizardComponent implements OnInit, CanComponentDeactivate {
     } else {
       this.wizardComponent.goToStep(this.wizardComponent.currentStepIndex - 2);
     }
+  }
+
+  selectedFisionomy(e) {
+    this.selectedRadioBtn(e);
+  }
+
+  selectedPrimaryObjective(e) {
+    /* assegno badge 1 se non è stato selezionata nessuna card
+     * assegno badge 2 se è stato selezionata già una card
+     * se clicco su una card già selezionata allora viene deselezionata
+     *  - se la card deselezionata era l'unica ad essere stata selezionata allora togli il badge 1
+     *  - se vi sono due card selezionate e una di queste viene deselezionata, allora viene aggiornato il badge a 1 di quella ancora selezionata
+    */
+    const firstChoiceEl = document.getElementsByClassName('firstChoice-icon')[0];
+    // const firstChoiceVal;
+    console.log(firstChoiceEl)
+    const secondChoiceEl = document.getElementsByClassName('secondChoice-icon')[0];
+
+    e.target.parentElement.insertAdjacentHTML('afterbegin', '<svg *ngIf="false" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-square-fill check-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>');
+
   }
 
   selectedRadioBtn(e) {
