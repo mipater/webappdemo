9f65ba362cdf2c3266d0b020eb35579365d12635
Michele Paternoster <michele.paternoster@finconsgroup.com>
2020-08-04 19:07:05 +0200

    continue work

--- src/app/main/ia-test/stepwizard/stepwizard.component.ts
+++ src/app/main/ia-test/stepwizard/stepwizard.component.ts
@@ -83,11 +83,18 @@ export class StepwizardComponent implements OnInit, CanComponentDeactivate {
      *  - se vi sono due card selezionate e una di queste viene deselezionata, allora viene aggiornato il badge a 1 di quella ancora selezionata
     */
     const firstChoiceEl = document.getElementsByClassName('firstChoice-icon')[0];
-    // const firstChoiceVal;
-    console.log(firstChoiceEl)
     const secondChoiceEl = document.getElementsByClassName('secondChoice-icon')[0];
+    let badgeToApply = null;
 
-    e.target.parentElement.insertAdjacentHTML('afterbegin', '<svg *ngIf="false" width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-check-square-fill check-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg"> <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/></svg>');
+    if (!firstChoiceEl) {
+      badgeToApply = 'firstChoice-icon';
+    } else if (firstChoiceEl && !secondChoiceEl) {
+      badgeToApply = 'secondChoice-icon';
+    }
+
+    if (badgeToApply) {
+      e.target.parentElement.insertAdjacentHTML('afterbegin', '<div class="' + badgeToApply +'"></div>');
+    }
 
   }
 
