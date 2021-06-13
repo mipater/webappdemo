898f617eaa5b5dd32c2453747f53a571fd517490
Michele Paternoster <pianomaik@gmail.com>
2021-06-13 21:12:42 +0200

    chatbot

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const webhook = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/webappdemo'));
@@ -12,13 +11,5 @@ app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/webappdemo/index.html'));
 });
 
-webhook.get('/*', (req, res) => {
-  var resp = 'This is the response';
-  let responseJson = {};
-  responseJson.fulfillmentText = 'This is an endpoint published to RunKit'; // displayed response
-  res.send(resp);
-})
-
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-webhook.listen(8081);
